4001eff54e1efcc7667e4b616de26ad8188e143d
phooc <bjoe7382@gmail.com>
2024-11-16 23:56:09 -0600

    added backEnd

---
+++ backend/pinata-service.ts
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class pinata-service extends PureComponent {
+  render() {
+    return (
+      <div>pinata-service</div>
+    )
+  }
+}
+
+export default pinata-service
